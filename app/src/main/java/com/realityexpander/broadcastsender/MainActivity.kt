20e70654043d9757e8db5f7b43dc6dd45083f8d4
Chris Athanas <realityexpander@gmail.com>
2022-08-24 04:25:23 -0500

    Improve comments

--- app/src/main/java/com/realityexpander/broadcastsender/MainActivity.kt
+++ app/src/main/java/com/realityexpander/broadcastsender/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+// Video Series: // Video series: https://www.youtube.com/watch?v=prueLyTvOwI&list=PLrnPJCHvNZuBqr_0AS9BPXgU6gvNeai5S
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     fun sendBroadcast(v: View?) {
         val intent = Intent("com.realityexpander.EXAMPLE_ACTION")
-        intent.setPackage("com.realityexpander.broadcastreceiver")
+        intent.setPackage("com.realityexpander.broadcastreceiver") // limits scope to our app only
         val extras = Bundle()
         extras.putString("stringExtra", "Start")
 
@@ -50,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                 textView?.text = result
             },
 
-            null,
+            null,      // Make not-null to run on a thread other than the main thread
             0,        // `resultCode` in the receiver
             "Start",  // `resultData` in the receiver
             extras              // `getResultExtras` in the receiver
